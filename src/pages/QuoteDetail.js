2e08ed68df89ec1b4c081eae2cd8e13c7b858b05
Sayed <nuruddinsayeed@gmail.com>
2021-09-11 02:43:57 +0600

    quote comment Route added

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,6 @@
 import { useParams } from "react-router-dom"
+import { Route } from "react-router-dom"
+import Comments from "../components/comments/Comments"
 
 
 const QuoteDetail = () => {
@@ -8,6 +10,9 @@ const QuoteDetail = () => {
         <div>
             <h1>Quote Detail</h1>
             <p>Quote Id: {params.quoteId}</p>
+            <Route path={`/quotes/${params.quoteId}/comments`}>
+                <Comments />
+            </Route>
         </div>
     )
 }
